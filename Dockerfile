c37eae888fcfd2f758520b1f8c76d3c79b91dc81
azarouski <azarouski@solvd.com>
2024-03-21 18:13:40 +0300

    refactoring was performed

--- Dockerfile
+++ Dockerfile
@@ -1,56 +1,41 @@
 FROM alpine:3.19.1
 
-ENV DEBIAN_FRONTEND=noninteractive
-
-ENV DEVICE_UDID=''
-ENV DEVICE_BUS=/dev/bus/usb/003/011
-
-ENV POLLING_SEC=5
-
-# Debug mode vars
-ENV DEBUG=false
-ENV DEBUG_TIMEOUT=3600
-ENV VERBOSE=false
-
-# iOS envs
-ENV WDA_HOST=localhost
-ENV WDA_PORT=8100
-ENV MJPEG_PORT=8101
-ENV WDA_WAIT_TIMEOUT=30
-ENV WDA_LOG_FILE=/tmp/log/wda.log
-ENV WDA_BUNDLEID=com.facebook.WebDriverAgentRunner.xctrunner
-ENV WDA_FILE=/tmp/zebrunner/WebDriverAgent.ipa
-
-RUN mkdir /tmp/log/; \
-    mkdir /tmp/zebrunner/
-
-# Usbmuxd settings "host:port"
-ENV USBMUXD_SOCKET_ADDRESS=''
-
-COPY debug.sh /opt
+ENV DEBIAN_FRONTEND=noninteractive \
+    DEVICE_UDID='' \
+    DEVICE_BUS=/dev/bus/usb/003/011 \
+    POLLING_SEC=5 \
+    # Debug mode vars
+    DEBUG=false \
+    DEBUG_TIMEOUT=3600 \
+    VERBOSE=false \
     # Logger
     LOGGER_LEVEL=INFO \
+    # iOS envs
+    WDA_HOST=localhost \
+    WDA_PORT=8100 \
+    MJPEG_PORT=8101 \
+    WDA_WAIT_TIMEOUT=30 \
+    WDA_LOG_FILE=/tmp/log/wda.log \
+    WDA_BUNDLEID=com.facebook.WebDriverAgentRunner.xctrunner \
+    WDA_FILE=/tmp/zebrunner/WebDriverAgent.ipa \
+    # Usbmuxd settings "host:port"
+    USBMUXD_SOCKET_ADDRESS=''
 
-RUN apk add --no-cache bash
-
-# busybox-extras include (unzip, wget, iputils-ping (ping), nc) packages
-RUN apk add --no-cache nano jq curl socat libc6-compat busybox-extras; \
-    apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing usbmuxd
-
-#=============
-# Set WORKDIR
-#=============
 WORKDIR /root
 
-#Grab gidevice from github and extract it in a folder
-RUN wget https://github.com/danielpaulus/go-ios/releases/download/v1.0.120/go-ios-linux.zip
-# https://github.com/danielpaulus/go-ios/releases/latest/download/go-ios-linux.zip
-RUN unzip go-ios-linux.zip -d /usr/local/bin
+RUN mkdir /tmp/log/ ;\
+    mkdir /tmp/zebrunner/ ;\
+    # busybox-extras include (unzip, wget, iputils-ping (ping), nc) packages
+    apk add --no-cache bash nano jq curl socat libc6-compat busybox-extras ;\
+    apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing usbmuxd ;\
+    # Grab go-ios from github and extract it in a folder
+    # https://github.com/danielpaulus/go-ios/releases/latest/download/go-ios-linux.zip
+    wget https://github.com/danielpaulus/go-ios/releases/download/v1.0.121/go-ios-linux.zip &&\
+    unzip go-ios-linux.zip -d /usr/local/bin &&\
+    rm -f go-ios-linux.zip &&\
+    ios --version
 
-RUN ios --version
-
-# Copy entrypoint script
 COPY logger.sh /opt
+COPY debug.sh /opt
 COPY entrypoint.sh /
-
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,9 @@
 . /opt/debug.sh
 . /opt/logger.sh
 
-
 #### Prepare for connection
 if [[ -z $USBMUXD_SOCKET_ADDRESS ]]; then
-  echo "Start containerized usbmuxd service/process"
+  logger "INFO" "Start containerized usbmuxd service/process"
   usbmuxd -f &
   sleep 2
   # socat server to share usbmuxd socket via TCP
@@ -20,129 +19,84 @@ fi
 
 #### Check the connection
 declare -i index=0
-available=0
-while [[ $available -eq 0 ]] && [[ $index -lt 10 ]]; do
-  available=$(ios list | grep -c $DEVICE_UDID)
-  if [[ $available -eq 1 ]]; then
-    echo "Device '$DEVICE_UDID' is available."
+while [[ $index -lt 10 ]]; do
+  if deviceInfo=$(ios info --udid="$DEVICE_UDID" 2>&1); then
+    logger "INFO" "Device '$DEVICE_UDID' is available."
+    logger "INFO" "Device info:\n$deviceInfo"
     break
-  fi
-  echo "Can't find UDID '$DEVICE_UDID' in 'ios list'. Waiting for ${POLLING_SEC} seconds."
-  sleep ${POLLING_SEC}
-  index+=1
-done
-
-if [[ $available -ne 1 ]]; then
-  echo "Device is not available. Restarting."
-  exit 1
-fi
-
-
-#### Detect device parameters
-echo "[$(date +'%d/%m/%Y %H:%M:%S')] populating device info"
-deviceInfo=$(ios info --udid=$DEVICE_UDID 2>&1)
-echo "device info: $deviceInfo"
-
-if [[ "${deviceInfo}" == *"failed getting info"* ]]; then
-  echo "ERROR! failed getting info. No sense to proceed with services startup!"
-  exit 0
-fi
-
-export PLATFORM_VERSION=$(echo $deviceInfo | jq -r ".ProductVersion | select( . != null )")
-deviceClass=$(echo $deviceInfo | jq -r ".DeviceClass | select( . != null )")
-export DEVICETYPE='Phone'
-if [ "$deviceClass" = "iPad" ]; then
-  export DEVICETYPE='Tablet'
-fi
-if [ "$deviceClass" = "AppleTV" ]; then
-  export DEVICETYPE='tvOS'
-fi
-
-echo "Detected device characteristics:"
-echo "PLATFORM_VERSION=$PLATFORM_VERSION"
-echo "deviceClass=$deviceClass"
-echo "DEVICETYPE=$DEVICETYPE"
-
-# Parse output to detect Timeoud out error.
-# {"channel_id":"com.apple.instruments.server.services.deviceinfo","error":"Timed out waiting for response for message:5 channel:0","level":"error","msg":"failed requesting channel","time":"2023-09-05T15:19:27Z"}
-
-if [[ "${deviceInfo}" == *"Timed out waiting for response for message"* ]]; then
-  echo "ERROR! Timed out waiting for response detected."
-  if [[ "${DEVICETYPE}" == "tvOS" ]]; then
-    echo "ERROR! TV reboot is required! Exiting without restart..."
+  elif [[ "${deviceInfo}" == *"failed getting info"* ]]; then
+    logger "ERROR" "Failed getting info. No sense to proceed with services startup!"
+    exit 0
+  elif [[ "${deviceInfo}" == *"Timed out waiting for response for message"* ]]; then
+    # Timed out error.
+    # {"channel_id":"com.apple.instruments.server.services.deviceinfo","error":"Timed out waiting for response for message:5 channel:0","level":"error","msg":"failed requesting channel","time":"2023-09-05T15:19:27Z"}
+    logger "ERROR" "Timed out waiting for response. Device reboot is recommended!"
     exit 0
   else
-    echo "WARN! device reboot is recommended!"
+    logger "WARN" "Can't find UDID '$DEVICE_UDID' in 'ios list'."
   fi
+
+  logger "WARN" "Waiting for ${POLLING_SEC} seconds."
+  sleep "${POLLING_SEC}"
+  index+=1
+done
+if [[ $index -ge 10 ]]; then
+  logger "ERROR" "Device is not available:\n$deviceInfo\nRestarting!"
+  exit 1
 fi
 
 
 #### Mount DeveloperDiscImage
-if [[ "${PLATFORM_VERSION}" == "17."* ]] || [[ "${DEVICETYPE}" == "tvOS" ]]; then
-  echo "Mounting iOS via Linux container not supported! WDA should be compiled and started via xcode!"
-  echo "wda install and startup steps will be skipped from appium container..."
-
-  # start proxy forward to device
-  ios forward $WDA_PORT $WDA_PORT --udid=$DEVICE_UDID >/dev/null 2>&1 &
-  ios forward $MJPEG_PORT $MJPEG_PORT --udid=$DEVICE_UDID >/dev/null 2>&1 &
-  return 0
-fi
-
-echo "[$(date +'%d/%m/%Y %H:%M:%S')] Allow to download and mount DeveloperDiskImages automatically"
-res=$(ios image auto --basedir /tmp/DeveloperDiskImages --udid=$DEVICE_UDID 2>&1)
-echo $res
-
+logger "INFO" "Allow to download and mount DeveloperDiskImages automatically"
 # Parse error to detect anomaly with mounting and/or pairing. It might be use case when user cleared already trusted computer
 # {"err":"failed connecting to image mounter: Could not start service:com.apple.mobile.mobile_image_mounter with reason:'SessionInactive'. Have you mounted the Developer Image?","image":"/tmp/DeveloperDiskImages/16.4.1/DeveloperDiskImage.dmg","level":"error","msg":"error mounting image","time":"2023-08-04T11:25:53Z","udid":"d6afc6b3a65584ca0813eb8957c6479b9b6ebb11"}
-
-if [[ "${res}" == *"error mounting image"* ]]; then
-  echo "ERROR! Mounting is broken due to the invalid paring. Please re pair again!"
-  exit 1
-else
-  echo "Developer Image auto mount succeed."
+if res=$(ios image auto --basedir /tmp/DeveloperDiskImages --udid="$DEVICE_UDID" 2>&1); then
+  logger "INFO" "Developer Image auto mount succeed:\n$res"
   sleep 3
+elif [[ "${res}" == *"error mounting image"* ]]; then
+  logger "ERROR" "Developer Image mounting is broken:\n$res\nRestarting!"
+  exit 1
 fi
 
 
 #### Check if WDA is already installed
-ios apps --udid=$DEVICE_UDID | grep -v grep | grep $WDA_BUNDLEID >/dev/null 2>&1
-if [[ ! $? -eq 0 ]]; then
-  echo "$WDA_BUNDLEID app is not installed"
+if ios apps --udid="$DEVICE_UDID" | grep -v grep | grep "$WDA_BUNDLEID" > /dev/null 2>&1; then
+  logger "INFO" "'$WDA_BUNDLEID' app is already installed"
+else
+  logger "INFO" "'$WDA_BUNDLEID' app is not installed"
 
   if [ ! -f $WDA_FILE ]; then
-    echo "ERROR! WebDriverAgent.ipa file is not exist or not a regular file!"
+    logger "ERROR" "'$WDA_FILE' file is not exist or not a regular file. Exiting!"
     exit 0
   fi
 
-  echo "[$(date +'%d/%m/%Y %H:%M:%S')] Installing WDA application on device"
-  ios install --path="$WDA_FILE" --udid=$DEVICE_UDID
-  if [ $? == 1 ]; then
-    echo "ERROR! Unable to install WebDriverAgent.ipa!"
+  logger "INFO" "Installing WDA application on device:"
+  ios install --path="$WDA_FILE" --udid="$DEVICE_UDID"
+  if [ $? -eq 1 ]; then
+    logger "ERROR" "Unable to install '$WDA_FILE'. Exiting!"
     exit 0
   fi
-else
-  echo "$WDA_BUNDLEID app is already installed"
 fi
 
 
 #### Start WDA
 # no need to launch springboard as it is already started. below command doesn't activate it!
-#echo "[$(date +'%d/%m/%Y %H:%M:%S')] Activating default com.apple.springboard during WDA startup"
+#logger "INFO" "Activating default com.apple.springboard during WDA startup"
 #ios launch com.apple.springboard
-touch ${WDA_LOG_FILE}
+touch "${WDA_LOG_FILE}"
 # verify if wda is already started and reuse this session
-curl -Is "http://${WDA_HOST}:${WDA_PORT}/status" | head -1 | grep -q '200 OK'
-if [ $? -eq 1 ]; then
-  echo "Existing WDA not detected"
 
-  schema=WebDriverAgentRunner
-  if [ "$DEVICETYPE" == "tvOS" ]; then
-    schema=WebDriverAgentRunner_tvOS
-  fi
+if ! curl -Is "http://${WDA_HOST}:${WDA_PORT}/status"; then
+  logger "INFO" "Existing WDA not detected."
 
   #Start the WDA service on the device using the WDA bundleId
-  echo "[$(date +'%d/%m/%Y %H:%M:%S')] Starting WebDriverAgent application on port $WDA_PORT"
-  ios runwda --bundleid=$WDA_BUNDLEID --testrunnerbundleid=$WDA_BUNDLEID --xctestconfig=${schema}.xctest --env USE_PORT=$WDA_PORT --env MJPEG_SERVER_PORT=$MJPEG_PORT --env UITEST_DISABLE_ANIMATIONS=YES --udid=$DEVICE_UDID > ${WDA_LOG_FILE} 2>&1 &
+  logger "INFO" "Starting WebDriverAgent application on port $WDA_PORT"
+  ios runwda \
+    --bundleid=$WDA_BUNDLEID \
+    --env USE_PORT=$WDA_PORT \
+    --env MJPEG_SERVER_PORT=$MJPEG_PORT \
+    --env UITEST_DISABLE_ANIMATIONS=YES \
+    --udid=$DEVICE_UDID > ${WDA_LOG_FILE} 2>&1 &
 
   # #148: ios: reuse proxy for redirecting wda requests through appium container
   ios forward $WDA_PORT $WDA_PORT --udid=$DEVICE_UDID >/dev/null 2>&1 &
@@ -154,22 +108,19 @@ tail -f ${WDA_LOG_FILE} &
 
 #### Wait for WDA start
 startTime=$(date +%s)
-idleTimeout=$WDA_WAIT_TIMEOUT
 wdaStarted=0
-while [ $((startTime + idleTimeout)) -gt "$(date +%s)" ]; do
-  curl -Is "http://${WDA_HOST}:${WDA_PORT}/status" | head -1 | grep -q '200 OK'
-  if [ $? -eq 0 ]; then
-    echo "Wda status is ok."
+while [ $(( startTime + WDA_WAIT_TIMEOUT )) -gt "$(date +%s)" ]; do
+  if resp=$(curl -Is "http://${WDA_HOST}:${WDA_PORT}/status"); then
+    logger "INFO" "Wda started successfully:\n$resp"
     wdaStarted=1
     break
   fi
-  sleep 1
+  logger "WARN" "Bad or no response from 'http://${WDA_HOST}:${WDA_PORT}/status':\n$resp\nOne more attempt."
+  sleep 2
 done
 
 if [ $wdaStarted -eq 0 ]; then
-  echo "WDA is unhealthy!"
-  # Destroy appium process as there is no sense to continue with undefined WDA_HOST ip!
-  pkill node
+  logger "ERROR" "No response from WDA, or WDA is unhealthy!. Restarting!"
   exit 1
 fi
 
@@ -180,7 +131,7 @@ fi
 
 
 #### Healthcheck
-echo "Connecting to ${WDA_HOST} ${MJPEG_PORT} using netcat..."
+logger "INFO" "Connecting to ${WDA_HOST} ${MJPEG_PORT} using netcat..."
 nc ${WDA_HOST} ${MJPEG_PORT}
-echo "${WDA_HOST} ${MJPEG_PORT} connection closed. Restarting."
+logger "WARN" "${WDA_HOST} ${MJPEG_PORT} connection closed. Restarting."
 exit 1
