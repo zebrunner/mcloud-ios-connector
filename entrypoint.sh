96b5a7e899ebb298440e5cfc3881f8619f837b85
azarouski <azarouski@solvd.com>
2024-07-02 15:05:34 +0200

    some checks to improve usbmuxd forwarding state were added

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,18 +6,95 @@
 logger "INFO" "\n\n\n\t\tMCLOUD-IOS-CONNECTOR\n\n"
 
 
-#### Prepare for connection
+#### Establish and check usbmuxd connection
+# Check if 2222 port is free
+declare -i index=0
+isLocalPortFree=0
+while [[ $index -lt 10 ]]; do
+  # If we can connect to the port, then someone is already waiting for a connection on this port
+  if nc -z localhost 2222; then
+    logger "ERROR" "localhost 2222 port is busy. One more attempt..."
+    index+=1
+    sleep 1
+  else
+    isLocalPortFree=1
+    break
+  fi
+done
+index=0
+if [[ $isLocalPortFree -eq 1 ]]; then
+  logger "localhost 2222 port is free."
+else
+  logger "ERROR" "localhost 2222 port is busy. Exiting!"
+  exit 0
+fi
+
+# Check if selected usbmuxd socket available
+declare -i index=0
+isUsbmuxdConnected=0
 if [[ -z $USBMUXD_SOCKET_ADDRESS ]]; then
   logger "Start containerized usbmuxd service/process"
   usbmuxd -f &
-  sleep 2
-  # socat server to share usbmuxd socket via TCP
+  # Check if '/var/run/usbmuxd' exists
+  while [[ $index -lt 10 ]]; do
+    if ! socat /dev/null UNIX-CONNECT:/var/run/usbmuxd; then
+      logger "ERROR" "Can't connect to '/var/run/usbmuxd'. One more attempt..."
+      index+=1
+      sleep 1
+    else
+      isUsbmuxdConnected=1
+      break
+    fi
+  done
+  index=0
   socat TCP-LISTEN:2222,reuseaddr,fork UNIX-CONNECT:/var/run/usbmuxd &
 else
   # rm /var/run/usbmuxd in advance to be able to start socat and join it to $USBMUXD_SOCKET_ADDRESS
   # rm -f /var/run/usbmuxd
   # socat UNIX-LISTEN:/var/run/usbmuxd,fork,reuseaddr,mode=777 TCP:"$USBMUXD_SOCKET_ADDRESS" &
+
+  # Check if 'USBMUXD_SOCKET_ADDRESS' exists
+  while [[ $index -lt 10 ]]; do
+    if ! socat /dev/null TCP:"$USBMUXD_SOCKET_ADDRESS"; then
+      logger "ERROR" "Can't connect to USBMUXD_SOCKET_ADDRESS: '$USBMUXD_SOCKET_ADDRESS'. One more attempt..."
+      index+=1
+      sleep 1
+    else
+      isUsbmuxdConnected=1
+      break
+    fi
+  done
+  index=0
   socat TCP-LISTEN:2222,reuseaddr,fork TCP:"$USBMUXD_SOCKET_ADDRESS" &
+fi
+
+if [[ $isUsbmuxdConnected -eq 1 ]]; then
+  logger "Usbmuxd socket is available."
+else
+  logger "ERROR" "Usbmuxd socket is not available. Exiting!"
+  exit 0
+fi
+
+# Check if localhost 2222 port is accessible now
+declare -i index=0
+isPortAccessible=0
+while [[ $index -lt 10 ]]; do
+  if ! nc -z localhost 2222; then
+    logger "ERROR" "Usbmuxd forwarding is not established. One more attempt..."
+    index+=1
+    sleep 1
+  else
+    isPortAccessible=1
+    break
+  fi
+done
+index=0
+
+if [[ $isPortAccessible -eq 1 ]]; then
+  logger "Usbmuxd forwarding established."
+else
+  logger "ERROR" "Usbmuxd forwarding is not established. Exiting!"
+  exit 0
 fi
 
 
