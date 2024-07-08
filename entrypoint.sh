404d188406b7f38a20fc1c58a126f0a760f79c47
azarouski <azarouski@solvd.com>
2024-07-08 14:00:22 +0200

    socat forwarding command was moved

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,10 +43,10 @@ if [[ -z $USBMUXD_SOCKET_ADDRESS ]]; then
       sleep 1
     else
       isUsbmuxdConnected=1
+      socat TCP-LISTEN:2222,reuseaddr,fork UNIX-CONNECT:/var/run/usbmuxd &
       break
     fi
   done; index=0
-  socat TCP-LISTEN:2222,reuseaddr,fork UNIX-CONNECT:/var/run/usbmuxd &
 else
   # rm /var/run/usbmuxd in advance to be able to start socat and join it to $USBMUXD_SOCKET_ADDRESS
   # rm -f /var/run/usbmuxd
@@ -60,10 +60,10 @@ else
       sleep 1
     else
       isUsbmuxdConnected=1
+      socat TCP-LISTEN:2222,reuseaddr,fork TCP:"$USBMUXD_SOCKET_ADDRESS" &
       break
     fi
   done; index=0
-  socat TCP-LISTEN:2222,reuseaddr,fork TCP:"$USBMUXD_SOCKET_ADDRESS" &
 fi
 
 if [[ $isUsbmuxdConnected -eq 1 ]]; then
@@ -95,7 +95,7 @@ else
 fi
 
 
-#### Check the connection
+#### Check device connection
 declare -i index=0
 isAvailable=0
 while [[ $index -lt 10 ]]; do
